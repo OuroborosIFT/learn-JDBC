8d5d4c919c3418d95cb041d6a89d881f98dea4ac
Arsik <arsik.magomedov18@gmail.com>
2023-02-20 21:30:01 +0300

    Separate two main methods

---
+++ src/main/java/com/ars/db/contacts/ConsoleMain.java
@@ -0,0 +1,10 @@
+package com.ars.db.contacts;
+
+import com.ars.db.contacts.interaction.Interaction;
+
+public class ConsoleMain {
+    public static void main(String[] args) {
+        Interaction interact = new Interaction();
+        interact.session();
+    }
+}
--- src/main/java/com/ars/db/contacts/WebMain.java
+++ src/main/java/com/ars/db/contacts/WebMain.java
@@ -1,18 +1,11 @@
 package com.ars.db.contacts;
 
-import com.ars.db.contacts.interaction.Interaction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ContactMain {
+public class WebMain {
     public static void main(String[] args) {
-        SpringApplication.run(ContactMain.class, args);
-
-	Interaction interact = new Interaction();
-
-	interact.session();
-
-
+        SpringApplication.run(WebMain.class, args);
     }
 }
